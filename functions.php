d7745c1a50780c6dbc7583fa256774d355478cdf
demid <dmitrii8155@icloud.com>
2025-12-26 12:40:23 +0300

    save

--- functions.php
+++ functions.php
@@ -4,12 +4,12 @@ add_action('wp_enqueue_scripts', function () {
 
   wp_enqueue_style(
     'style',
-    get_template_directory_uri() . '/assets/css/main.css' // Основной css файл
+    get_template_directory_uri() . '/assets/css/main.css' // Основной css 
   );
 
   wp_enqueue_script(
     'script',
-    get_template_directory_uri() . '/assets/js/main.js', // Основной js файл
+    get_template_directory_uri() . '/assets/js/main.js', // Основной js
     [],
     null,
     true
